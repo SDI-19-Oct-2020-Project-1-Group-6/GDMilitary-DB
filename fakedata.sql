525c96799230003e5e71bdb8c28f73c8079c438a
brandonjohnson21 <brandon.johnson.21@us.af.mil>
2020-11-24 14:57:47 -0800

    corrolate fake data

--- fakedata.sql
+++ fakedata.sql
@@ -1,11 +1,10 @@
 SELECT addAFSC('3d1x1','Computers or Something');
-SELECT addUnit('24NOS','BFE',5,NULL);
-SELECT addUnit('194CWS','Seattle, WA',10,array['1b4x4','3d1x1']);
+SELECT addUnit('24NOS','BFE',2,NULL);
+SELECT addUnit('1AF','Tyndall AFB, FL',70,array['1b4x4','3d1x1']);
 
 INSERT INTO people (name) VALUES ('Aviery'),('Brandon'),('Caden'),('Dameon');
 
 INSERT INTO reviews (person_id, reviewStars, pros, cons, unit_id)
-    VALUES (1, 3, 'popcorn', 'everything', '726'),
-           (2, 5, 'everything', 'no dr.pepper', '603'),
-           (3, 1, 'i get to tdy', 'i have to work', '460');
-           
+    VALUES (1, 3, 'popcorn', 'everything', '24NOS'),
+           (2, 5, 'everything', 'no dr.pepper', '24NOS'),
+           (3, 1, 'i get to tdy', 'i have to work', '1AF');
